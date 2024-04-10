c843801663e7190b65de085a0832ab771793f987
etokNat <etoknathaniel@gmail.com>
2024-04-10 15:42:19 +0100

    create new filebto implement error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import bcryptjs from "bcryptjs"
 import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
 
   if (
@@ -12,16 +13,14 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required." })
+    next(errorHandler(400, "All Fields are required."))
   }
 
   try {
     const existingUser = await User.findOne({ email })
 
     if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User with this email already exists." })
+      next(errorHandler(400, "User with this email already exists."))
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 14)
@@ -32,6 +31,6 @@ export const signup = async (req, res) => {
     return res.json({ message: "Signup successful" })
   } catch (error) {
     console.error(error)
-    return res.status(500).json({ message: "Internal server error" })
+    return next(error)
   }
 }
