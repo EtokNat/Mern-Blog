f36257bc46db679bccff30772d1406b038f49722
etokNat <etoknathaniel@gmail.com>
2024-04-16 16:41:59 +0100

    add user state persistence using redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import { persistReducer, persistStore } from "redux-persist"
 import userReducer from "./user/userSlice"
+import storage from"redux-persist/lib/storage"
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1
+}
+
+const rootReducer = combineReducers({
+  user: userReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  }
+  reducer: 
+     persistedReducer,
+    middleware: getDwfaultMiddleware =>
+      getDwfaultMiddleware({ serializableCheck: false })
+  
 })
+
+export const persistor = persistStore(store)
